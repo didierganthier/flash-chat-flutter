0518594e6fae784119dc62bdb74fe8aa24cd4b48
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-05-20 22:30:10 -0400

    Scrolling to last message

--- lib/screens/messages_stream.dart
+++ lib/screens/messages_stream.dart
@@ -23,7 +23,7 @@ class MessagesStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data.documents;
+        final messages = snapshot.data.documents.reversed;
         List<MessageBubble> messagesWidgets = [];
         for(var message in messages){
           final messageText = message.data['text'];
@@ -42,6 +42,7 @@ class MessagesStream extends StatelessWidget {
         }
         return Expanded(
           child: ListView(
+            reverse: true,
             padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
             children: messagesWidgets,
           ),
